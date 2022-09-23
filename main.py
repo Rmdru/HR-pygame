6e33cce4355f2544a7dad5aee639be3407739c8a
rmdru <rmdruijter@gmail.com>
2022-09-23 11:40:57 +0200

    Application base

---
+++ main.py
@@ -0,0 +1,30 @@
+#imports
+import pygame
+
+#initializing pygame
+pygame.init()
+
+#vars
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+BACKGROUND_COLOR = (0, 0, 0)
+
+#application window name
+pygame.display.set_caption("Werkplaats 1: PyGame Nohtyp")
+
+#set application window size
+canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+#function to check if user has requested to quit game
+def quit_game_requested():
+    halting = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            halting = True
+            break
+    return halting
+
+#loop that runs every frame
+while not quit_game_requested():
+    #bg color
+    canvas.fill(BACKGROUND_COLOR)
