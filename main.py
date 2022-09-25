3ee99c681f85a1ecdd5125154aa225153f97706c
rmdru <1056362@hr.nl>
2022-09-25 15:59:15 +0200

    fixed spawn and movement of targets

--- main.py
+++ main.py
@@ -7,16 +7,21 @@ import random
 pygame.init()
 
 #vars
+GAME_SPEED = 60
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 BACKGROUND_COLOR = (0, 0, 0)
 TARGET_AMOUNT = 5
-TARGET_SPEED = 10
+TARGET_SPEED = [-5, 0]
 TARGET_SPAWN_INTERVAL = 1000
 TARGET_SIZE = 50
+TARGET_SPAWN_AREA_WIDTH = 0.5
 
 #application window name
 pygame.display.set_caption("Werkplaats 1: PyGame Nohtyp")
+
+#start pygame clock
+clock = pygame.time.Clock()
 
 #set application window size
 canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -30,17 +35,28 @@ def quit_game_requested():
             break
     return halting
 
-#spawn targets
+#generate target surfaces
+targets_surface = []
+targets_color = []
 for i in range(TARGET_AMOUNT):
-    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-    spawn_area_width = SCREEN_WIDTH / 2
-    pygame.draw.rect(canvas, color, pygame.Rect(random.randint(spawn_area_width, SCREEN_WIDTH - TARGET_SIZE), random.randint(0, SCREEN_HEIGHT - TARGET_SIZE), TARGET_SIZE, TARGET_SIZE))
-    pygame.display.flip()
+    target_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    spawn_area_width = SCREEN_WIDTH * TARGET_SPAWN_AREA_WIDTH
+    target_surface = pygame.Rect(pygame.Rect(random.randint(spawn_area_width, SCREEN_WIDTH - TARGET_SIZE), random.randint(0, SCREEN_HEIGHT - TARGET_SIZE), TARGET_SIZE, TARGET_SIZE))
+    targets_surface.append(target_surface)
+    targets_color.append(target_color)
 
 #loop that runs every frame
 while not quit_game_requested():
     #bg color
     canvas.fill(BACKGROUND_COLOR)
 
+    #loop through target surfaces to draw them on the canvas
+    for index, item in enumerate(targets_surface):
+        item.move_ip(TARGET_SPEED)
+        pygame.draw.rect(canvas, targets_color[index], item)
+
     #update the display
-    # pygame.display.flip()
+    pygame.display.flip()
+
+    #wait for next clock tick
+    clock.tick(GAME_SPEED)
