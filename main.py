aacf0499fb31836acc7112639732de1b706f7640
Ruben-1056362 <1056362@hr.nl>
2022-09-28 17:35:18 +0200

    merge: merged startscreen from anant to main

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import pygame
 pygame.init()
 
 # vars
-SCREEN_WIDTH = 1280
-SCREEN_HEIGHT = 720
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
 BACKGROUND_COLOR = (0, 0, 0)
 
 # application window name
@@ -15,9 +15,12 @@ pygame.display.set_caption("Werkplaats 1: PyGame Nohtyp")
 # set application window size
 canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# application window icon
-logo = pygame.image.load("img/RuimteschipLogo.png").convert_alpha()
-logo_rect = logo.get_rect()
+# set application window icon
+logo = pygame.image.load("RuimteschipLogo.png")
+pygame.display.set_icon(logo)
+
+#start pygame clock
+clock = pygame.time.Clock()
 
 # function to check if user has requested to quit game
 def quit_game_requested():
@@ -30,89 +33,50 @@ def quit_game_requested():
 
 # loop that runs every frame
 while not quit_game_requested():
-    # bg color
-    canvas.fill(BACKGROUND_COLOR)
+    # Kleuren voor de schermachtergrond (wit) en de teksten (zwart)
+    zwart = (0, 0, 0)
+    wit = (255, 255, 255)
 
-    canvas.blit(logo, logo_rect)
+    # Kleuren van de startknop wanneer je wel/niet met de muis erover gaat
+    donker_groen = (0, 200, 0)
+    licht_groen = (0, 255, 0)
 
-    pygame.display.flip()
+    # Functies definiëren en uitvoeren
 
+    def tekst_instellingen(tekst, lettertype):
+        tekst_weergave = lettertype.render(tekst, True, zwart)
+        return tekst_weergave, tekst_weergave.get_rect()
 
-# Hier staat de code voor de score board
-def message_to_screen(message, color, font_size, x, y):
-    '''display messages to screen.'''
-    font = pygame.font.Sysfont(font_name, font_size)
-    text = font.render(message, True, color)
-    text_rect = text.get_rect()
-    text_rect = (x, y)
-    screen.blit(text_rect)
+    def startknop(mededeling, x ,y ,breedte, hoogte, inactivecolor, activecolor, action=None):
+        muis = pygame.mouse.get_pos()
 
-    message_to_screen("score:) " + str(score), WHITE, 24, SCREEN_WIDTH/2, 10)
+        if x + breedte > muis[0] > x and y + hoogte > muis[1] > y:
+            pygame.draw.rect(canvas, activecolor, (x, y, breedte, hoogte))
+        else:
+            pygame.draw.rect(canvas, inactivecolor, (x, y, breedte, hoogte))
 
-    bullet_collision = pygame.sprite.groupcollide(all_meteors, all_bullets, True, True)
-    for collision in bullet_collision:
-        spawn_new_meteor()
-        score += 150 - self.radius
+        tekst_in_kleine_letters = pygame.font.Font("freesansbold.ttf", 40)
+        tekst_uiterlijk, textRect = tekst_instellingen(mededeling, tekst_in_kleine_letters)
+        textRect.center = ((x + (breedte / 2)), (y + (hoogte / 2)))
+        canvas.blit(tekst_uiterlijk, textRect)
 
-# FIXME: Maak keuze qua taal (.EN), statische variabelen in HEADERS. Maak een keuze qua breedte. DdV
-# FIXME: Ik maak alleen maar problemen
+    def startscherm_game():
+        start = True
+        while start:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
 
-scherm_breedte = 800
-scherm_hoogte = 600
+            canvas.fill(wit)
+            tekst_in_hoofdletters = pygame.font.Font('freesansbold.ttf', 95)
+            tekst_uiterlijk, TextRect = tekst_instellingen("Space Shooter", tekst_in_hoofdletters)
+            TextRect.center = ((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
+            canvas.blit(tekst_uiterlijk, TextRect)
 
-# Kleuren voor de schermachtergrond (wit) en de teksten (zwart)
-zwart = (0, 0, 0)
-wit = (255, 255, 255)
+            startknop("Play!!!", 290, 380, 200, 100, donker_groen, licht_groen)
 
-# Kleuren van de startknop wanneer je wel/niet met de muis erover gaat
-donker_groen = (0, 200, 0)
-licht_groen = (0, 255, 0)
+            pygame.display.update()
+            clock.tick(15)
 
-# Balk boven het scherm - toegevoegde naam en logo
-startscherm = pygame.display.set_mode((scherm_breedte, scherm_hoogte))
-pygame.display.set_caption('Team: nohtyp')
-clock = pygame.time.Clock()
-
-logo = pygame.image.load("RuimteschipLogo.png")
-pygame.display.set_icon(logo)
-
-# Functies definiëren en uitvoeren
-
-def tekst_instellingen(tekst, lettertype):
-    tekst_weergave = lettertype.render(tekst, True, zwart)
-    return tekst_weergave, tekst_weergave.get_rect()
-
-def startknop(mededeling, x ,y ,breedte, hoogte, inactivecolor, activecolor, action=None):
-    muis = pygame.mouse.get_pos()
-    muisklik = pygame.mouse.get_pressed()
-
-    if x + breedte > muis[0] > x and y + hoogte > muis[1] > y:
-        pygame.draw.rect(startscherm, activecolor, (x, y, breedte, hoogte))
-    else:
-        pygame.draw.rect(startscherm, inactivecolor, (x, y, breedte, hoogte))
-
-    tekst_in_kleine_letters = pygame.font.Font("freesansbold.ttf", 40)
-    tekst_uiterlijk, textRect = tekst_instellingen(mededeling, tekst_in_kleine_letters)
-    textRect.center = ((x + (breedte / 2)), (y + (hoogte / 2)))
-    startscherm.blit(tekst_uiterlijk, textRect)
-
-def startscherm_game():
-    start = True
-    while start:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                quit()
-
-        startscherm.fill(wit)
-        tekst_in_hoofdletters = pygame.font.Font('freesansbold.ttf', 95)
-        tekst_uiterlijk, TextRect = tekst_instellingen("Space Shooter", tekst_in_hoofdletters)
-        TextRect.center = ((scherm_breedte / 2), (scherm_hoogte / 2))
-        startscherm.blit(tekst_uiterlijk, TextRect)
-
-        startknop("Play!!!", 290, 380, 200, 100, donker_groen, licht_groen)
-
-        pygame.display.update()
-        clock.tick(15)
-
-startscherm_game()
+    startscherm_game()
