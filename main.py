053143299aa6af4d225defe32e73cca1c0b38505
Ruben-1056362 <1056362@hr.nl>
2022-09-30 17:03:25 +0200

    trying to solve merge conflict

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 TARGET_AMOUNT = 5
-TARGET_SPEED = [-5, 0]
+TARGET_SPEED = [-3, 0]
 TARGET_SIZE = 50
 TARGET_SPAWN_AREA_WIDTH = 0.5
 BLACK = (0, 0, 0)
@@ -26,8 +26,8 @@ pygame.display.set_caption("Werkplaats 1: PyGame Nohtyp")
 canvas = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 # set application window icon
-logo = pygame.image.load("RuimteschipLogo.png")
-pygame.display.set_icon(logo)
+icon = pygame.image.load("nohtyp_logo_simplified.png")
+pygame.display.set_icon(icon)
 
 # start pygame clock
 clock = pygame.time.Clock()
@@ -68,6 +68,30 @@ def game_loop():
         targets_surface.append(target_surface)
         targets_color.append(target_color)
 
+    # Player
+    PlayerImg = pygame.image.load("rocket.png")
+    PlayerImg_X_size = 64
+    PlayerImg_Y_size = 64
+    playerX = SCREEN_WIDTH / 10
+    playerY = ( SCREEN_HEIGHT // 2 ) - ( PlayerImg_Y_size // 2 )
+    playerX_change = 0
+    playerY_change = 0
+    player_speed = 5
+
+    # Bullet
+    # Ready = Cant't see the bullet
+    # Fire = Bullet is moving on screen
+    BulletImg = pygame.image.load("bullet.png")
+    BulletImg_X_size = 32
+    BulletImg_Y_size = 32
+    bulletX = playerX
+    bulletY = playerY + (BulletImg_Y_size // 2)
+    bulletX_change = 1.5
+    bullet_state = "ready"
+
+    def player(x, y):
+        canvas.blit(PlayerImg, (x, y))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -89,6 +113,84 @@ def game_loop():
         for index, item in enumerate(targets_surface):
             item.move_ip(TARGET_SPEED)
             pygame.draw.rect(canvas, targets_color[index], item)
+
+        def fire_bullet(x, y):
+            global bullet_state
+            bullet_state = "fire"
+            canvas.blit(BulletImg, (x + (BulletImg_X_size // 2 ), y + (BulletImg_Y_size // 2)))
+
+        # Check events
+        for event in pygame.event.get():
+            # If keystroke is Pressed
+            if event.type == pygame.KEYDOWN:
+                ## Movement
+                print("Key down:")
+                # If a key, X decreases
+                if event.key == pygame.K_a:
+                    playerX_change = -player_speed
+                    print("a")
+                # If d key, X increases
+                if event.key == pygame.K_d:
+                    playerX_change = +player_speed
+                    print("d")
+                # If w key, Y decreases
+                if event.key == pygame.K_w:
+                    playerY_change = -player_speed
+                    print("w")
+                # If s key, Y increases
+                if event.key == pygame.K_s:
+                    playerY_change = +player_speed
+                    print("s")
+                
+                ## Shooting
+                if event.key == pygame.K_SPACE and bullet_state == "ready":
+                    # To make sure the bullet starts at the front of the player
+                    bulletX = playerX + 32
+                    # Set bullet Y coordinate to current player Y coordinate
+                    bulletY = playerY
+                    fire_bullet(playerX, bulletY)
+                    print("space")
+
+            # If keystroke is released, change speed to 0
+            if event.type == pygame.KEYUP:
+                print("Key up")
+                if event.key == pygame.K_a:
+                    playerX_change = 0
+                if event.key == pygame.K_d:
+                    playerX_change = 0
+                if event.key == pygame.K_w:
+                    playerY_change = 0
+                if event.key == pygame.K_s :
+                    playerY_change = 0
+
+        playerX += playerX_change
+        playerY += playerY_change
+
+        # Stop player if they hit the screen border
+        if playerX <= 0:
+            playerX = 0
+        if playerX >= SCREEN_WIDTH - PlayerImg_X_size:
+            playerX = SCREEN_WIDTH - PlayerImg_X_size
+        if playerY <= 0:
+            playerY = 0
+        if playerY >= SCREEN_HEIGHT - PlayerImg_Y_size:
+            playerY = SCREEN_HEIGHT - PlayerImg_Y_size
+
+        # Prevent player from going too far right
+        if playerX >= SCREEN_WIDTH * 0.60:
+            playerX = SCREEN_WIDTH * 0.60
+
+        # Bullet movement
+        if bullet_state == "fire":
+            fire_bullet(bulletX, bulletY)
+            bulletX += bulletX_change
+
+        # Reset bullet if it hits the border
+        if bulletX >= SCREEN_WIDTH:
+            bulletX = playerX
+            bullet_state = "ready"
+
+        player(playerX, playerY)
 
         pygame.display.update()
         clock.tick(GAME_SPEED)
